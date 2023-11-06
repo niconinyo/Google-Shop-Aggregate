95c1d7645c9029c70885d50a2a8e6c6f94621a66
winstonl <wrlucasas@gmail.com>
2023-11-06 08:57:22 -0800

    Added File structure

---
+++ Google-Shop-Aggregate/Frontend/src/componenets/App/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './styles.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Aggie</h1>
+      <p>Find the best price for you!</p>
+      
+    </>
+  )
+}
+
+export default App
